bf52b803a96ba96d4c5dce2f7c0a4b3e842683da
Spenser <spenser.davies@gmail.com>
2020-10-23 11:41:05 -0600

    remove favorite added and tooltips kinda working

--- src/components/recipes-list.component.js
+++ src/components/recipes-list.component.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import ReactTooltip from "react-tooltip";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { useAuth0 } from "@auth0/auth0-react";
@@ -73,7 +74,38 @@ function RecipeList(props) {
   };
 
   const removeFavorite = (id) => {
-    return;
+    console.log(id);
+    if (user) {
+      if (user.sub.length > 0) {
+        axios
+          .get("http://localhost:5000/recipes/" + id)
+          .then((res) => {
+            recipeToFavorite = res.data;
+            console.log("res.data: ");
+            console.log(res.data);
+            console.log("recipeToFavorite:");
+            console.log(recipeToFavorite);
+            let removalIndex = recipeToFavorite.favoritedBy.indexOf(user.sub);
+            console.log(removalIndex);
+            recipeToFavorite.favoritedBy.splice(removalIndex, 1);
+            console.log(recipeToFavorite);
+          })
+          .then(() => {
+            axios
+              .put("http://localhost:5000/recipes/" + id, recipeToFavorite)
+              .then(() => {
+                axios
+                  .get("http://localhost:5000/recipes")
+                  .then((res) => {
+                    setRecipes(res.data);
+                  })
+                  .catch((err) => {
+                    console.error(err);
+                  });
+              });
+          });
+      }
+    }
   };
 
   return (
@@ -95,6 +127,7 @@ function RecipeList(props) {
                     className="float-right text-warning"
                     onClick={() => removeFavorite(recipe._id)}
                     style={{ cursor: "pointer" }}
+                    data-tip
                   >
                     *
                   </h1>
@@ -105,11 +138,25 @@ function RecipeList(props) {
                     className="float-right"
                     onClick={() => addFavorite(recipe._id)}
                     style={{ cursor: "pointer" }}
+                    data-tip
+                    data-for="addTip"
                   >
                     *
                   </h1>
                 );
               }
+            } else {
+              favIcon = (
+                <h1
+                  className="float-right"
+                  onClick={() => addFavorite(recipe._id)}
+                  style={{ cursor: "pointer" }}
+                  data-tip
+                  data-for="addTip"
+                >
+                  *
+                </h1>
+              );
             }
           }
 
@@ -117,10 +164,19 @@ function RecipeList(props) {
           return (
             <div className="card shadow text-dark mb-5 col-10 mx-auto col-lg-4 col-md-6">
               <div className="card-body text-center">
+                <ReactTooltip id="removeFavoriteTip" place="top" effect="solid">
+                  Remove From Favorites
+                </ReactTooltip>
+                <ReactTooltip id="addTip" place="top" effect="solid">
+                  Add To Favorites
+                </ReactTooltip>
+
                 {favIcon}
-                <Link to={url} className="">
+                <Link to={url} className="text-dark">
                   <h4 className="card-title">{recipe.title}</h4>
-                  <p>{recipe.description}</p>
+                  <p>
+                    <i>{recipe.description}</i>
+                  </p>
 
                   <small>{recipe.serves} serving(s)</small>
                 </Link>
